3d9acee0a63db46b55ae020c70d37d6911b75cff
Paladian78 <singhyogendar78@gmail.com>
2023-01-02 10:37:19 +0530

    Update roles.tf

--- roles.tf
+++ roles.tf
@@ -69,7 +69,7 @@ data "azurerm_management_group" "level" {
     for key, value in try(var.role_mapping.built_in_role_mapping.management_group, {}) : key => value
   }
 
-  name = lower(each.key) == "root" ? data.azurerm_client_config.current.tenant_id : each.key
+  name = lower(each.key) == "root" ? "Tenant Root Group" : each.key
 }
 
 locals {
